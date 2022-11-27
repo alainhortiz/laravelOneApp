ba22addc2283832a6097f0915b77ad50882cf81b
alainhortiz <alainhortiz@gmail.com>
2022-11-26 20:03:31 -0500

    Creación de las rutas.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', static function () {
     return view('welcome');
+});
+
+Route::get('cursos', static function () {
+    return "Bienvenidos a la página de cursos";
+});
+
+Route::get('cursos/create', static function () {
+    return "En esta página podrás crear un curso";
+});
+
+//Route::get('cursos/{curso}', static function ($curso) {
+//    return "Bienvenidos al curso $curso";
+//});
+
+//Podemos definir que esta variable sea opcional con el signo ? y hay que inicializarla
+Route::get('cursos/{curso}/{categoria?}', static function ($curso, $categoria = null) {
+
+    if ($categoria) {
+        return "Bienvenidos al curso $curso, de la categoría $categoria";
+    }
+
+    return "Bienvenidos al curso $curso";
+
 });
